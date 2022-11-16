eb91bd83e169a1724315dd6e66d351c2ca4f9620
samwhitford11 <samantha.fichte98@gmail.com>
2022-11-16 14:03:01 -0700

    connects show index and the data

--- server.js
+++ server.js
@@ -3,16 +3,28 @@ const express = require('express');
 const app = express();
 const Budget = require("./Models/budget")
 
+
 const PORT = 3019
 
+app.use(express.static(__dirname + '/public')); 
 
-app.get("/", (req, res) => {
+//INDEX ROUTE
+app.get("/budgets", (req, res) => {
     res.render('budget_index.ejs', 
     {
         Budget: Budget
-    })
+    });
 });
 
+//SHOW ROUTE
+app.get("/budgets/:index", (req, res) => {
+    res.render('budget_show.ejs',
+    {
+        Budget: Budget[req.params.index],
+        index: req.params.index
+
+    });
+});
 
 
 
